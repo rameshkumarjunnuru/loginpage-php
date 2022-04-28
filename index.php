afbd25e1320f09f46fb97266260031e1c09da0bd
Ramesh Kumar Junnuru <48126950+rameshkumarjunnuru@users.noreply.github.com>
2022-04-28 18:17:41 +0530

    Updated index.php
    Added  New Login.php content

--- index.php
+++ index.php
@@ -1,121 +1,59 @@
+<html>
+<head>
+<title>Login </title>
+</head>
+<body>
+    <form name="form1" method="post">
+    <table align="center" width="80%" border="0" cellpadding="3" cellspacing="1">
+    <tr >
+        <td colspan=2 align="center">
+            <b><h2>Application User Login</h2></b>
+        </td>
+    </tr>
+        <br/>
+    <tr>
+        <td><b>Username <b> </td>
+        <td><input name="uname" type="text"></td>
+    </tr>
+    <tr>
+        <td><b>Password  <b></td>
+        <td><input name="pwd" type="password"></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td ><input type="submit" name="Submit" value="Login" style="background-color:green;color:white;width:80px;height:30px;"></td>
+    </tr>
+    </table>
+</form>
 <?php
-   ob_start();
-   session_start();
-?>
 
-<?
-   // error_reporting(E_ALL);
-   // ini_set("display_errors", 1);
-?>
-
-<html lang = "en">
+if(isset($_POST["Submit"]))
+{
+$uname=$_POST["uname"];
+$pwd=$_POST["pwd"];
+$xml=simplexml_load_file('users.xml') or die("Error: Cannot create object");
+foreach($xml->children() as $users) 
+{ 
+    if($users->userid==$uname)
+    {
+        if($users->password==$pwd)
+        {
+            echo "<center><b>Hello..".$uname; 
+            echo "<br/>";
+            echo "<br/>";
+            echo "Login Successful</b></center> ";
+            return;
+        }
+        else
+        {
+            echo "<center><b>Login failed due to incorrect username and password </b></center>";
+            return;
+        }
    
-   <head>
-      <title>Tutorialspoint.com</title>
-      <link href = "css/bootstrap.min.css" rel = "stylesheet">
-      
-      <style>
-         body {
-            padding-top: 40px;
-            padding-bottom: 40px;
-            background-color: #ADABAB;
-         }
-         
-         .form-signin {
-            max-width: 330px;
-            padding: 15px;
-            margin: 0 auto;
-            color: #017572;
-         }
-         
-         .form-signin .form-signin-heading,
-         .form-signin .checkbox {
-            margin-bottom: 10px;
-         }
-         
-         .form-signin .checkbox {
-            font-weight: normal;
-         }
-         
-         .form-signin .form-control {
-            position: relative;
-            height: auto;
-            -webkit-box-sizing: border-box;
-            -moz-box-sizing: border-box;
-            box-sizing: border-box;
-            padding: 10px;
-            font-size: 16px;
-         }
-         
-         .form-signin .form-control:focus {
-            z-index: 2;
-         }
-         
-         .form-signin input[type="email"] {
-            margin-bottom: -1px;
-            border-bottom-right-radius: 0;
-            border-bottom-left-radius: 0;
-            border-color:#017572;
-         }
-         
-         .form-signin input[type="password"] {
-            margin-bottom: 10px;
-            border-top-left-radius: 0;
-            border-top-right-radius: 0;
-            border-color:#017572;
-         }
-         
-         h2{
-            text-align: center;
-            color: #017572;
-         }
-      </style>
-      
-   </head>
-	
-   <body>
-      
-      <h2>Enter Username and Password</h2> 
-      <div class = "container form-signin">
-         
-         <?php
-            $msg = '';
-            
-            if (isset($_POST['login']) && !empty($_POST['username']) 
-               && !empty($_POST['password'])) {
-				
-               if ($_POST['username'] == 'Devops' && 
-                  $_POST['password'] == '1234') {
-                  $_SESSION['valid'] = true;
-                  $_SESSION['timeout'] = time();
-                  $_SESSION['username'] = 'tutorialspoint';
-                  
-                  echo 'You have entered valid use name and password';
-               }else {
-                  $msg = 'Wrong username or password';
-               }
-            }
-         ?>
-      </div> <!-- /container -->
-      
-      <div class = "container">
-      
-         <form class = "form-signin" role = "form" 
-            action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); 
-            ?>" method = "post">
-            <h4 class = "form-signin-heading"><?php echo $msg; ?></h4>
-            <input type = "text" class = "form-control" 
-               name = "username" placeholder = "username = Devops" 
-               required autofocus></br>
-            <input type = "password" class = "form-control"
-               name = "password" placeholder = "password = 1234" required>
-            <button class = "btn btn-lg btn-primary btn-block" type = "submit" 
-               name = "login">Login</button>
-         </form>
-			
-         Click here to clean <a href = "logout.php" tite = "Logout">Session.
-         
-      </div> 
-      
-   </body>
+    }
+}
+        echo "<center><b>Login failed due to incorrect username and password</b></center>";
+}
+?>
+</body>
 </html>
